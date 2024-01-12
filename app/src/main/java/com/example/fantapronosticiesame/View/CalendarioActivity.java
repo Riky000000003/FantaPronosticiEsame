d9a41eda40034c1be89e1ab914607c14ebac395f
Riccardo.Frigerio <134052922+Riky000000003@users.noreply.github.com>
2024-01-12 23:05:32 +0100

    agggiunto CalendarioActivity e ClassificaActivity

--- app/src/main/java/com/example/fantapronosticiesame/View/CalendarioActivity.java
+++ app/src/main/java/com/example/fantapronosticiesame/View/CalendarioActivity.java
@@ -8,33 +8,31 @@ import android.os.Bundle;
 
 import com.example.fantapronosticiesame.Model.Lega;
 import com.example.fantapronosticiesame.R;
-import com.example.fantapronosticiesame.databinding.ActivityLegaBinding;
+import com.example.fantapronosticiesame.databinding.ActivityCalendarioBinding;
 
-public class LegaActivity extends AppCompatActivity {
-    protected ActivityLegaBinding binding;
-
+public class CalendarioActivity extends AppCompatActivity {
+    protected ActivityCalendarioBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        binding = DataBindingUtil.setContentView(this, R.layout.activity_lega);
+        binding = DataBindingUtil.setContentView(CalendarioActivity.this, R.layout.activity_calendario);
         Lega lega = (Lega) getIntent().getSerializableExtra("lega");
-        binding.titoloLega.setText(lega.getNomeLega());
 
-        binding.tabView.setSelectedItemId(R.id.home);
+        binding.tabView.setSelectedItemId(R.id.calendario);
         binding.tabView.setOnItemSelectedListener(item -> {
             int itemId = item.getItemId();
             if (itemId == R.id.home) {
-                Intent intent = new Intent(LegaActivity.this, LegaActivity.class);
+                Intent intent = new Intent(CalendarioActivity.this, LegaActivity.class);
                 intent.putExtra("lega", lega);
                 startActivity(intent);
                 return true;
             } else if (itemId == R.id.calendario) {
-                Intent intent = new Intent(LegaActivity.this, CalendarioActivity.class);
+                Intent intent = new Intent(CalendarioActivity.this, CalendarioActivity.class);
                 intent.putExtra("lega", lega);
                 startActivity(intent);
                 return true;
             } else if (itemId == R.id.classifica) {
-                Intent intent = new Intent(LegaActivity.this, ClassificaActivity.class);
+                Intent intent = new Intent(CalendarioActivity.this, ClassificaActivity.class);
                 intent.putExtra("lega", lega);
                 startActivity(intent);
                 return true;
--- app/src/main/java/com/example/fantapronosticiesame/View/ClassificaActivity.java
+++ app/src/main/java/com/example/fantapronosticiesame/View/ClassificaActivity.java
@@ -8,35 +8,32 @@ import android.os.Bundle;
 
 import com.example.fantapronosticiesame.Model.Lega;
 import com.example.fantapronosticiesame.R;
-import com.example.fantapronosticiesame.databinding.ActivityLegaBinding;
+import com.example.fantapronosticiesame.databinding.ActivityClassificaBinding;
 
-public class LegaActivity extends AppCompatActivity {
-    protected ActivityLegaBinding binding;
-
+public class ClassificaActivity extends AppCompatActivity {
+    protected ActivityClassificaBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        binding = DataBindingUtil.setContentView(this, R.layout.activity_lega);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_classifica);
         Lega lega = (Lega) getIntent().getSerializableExtra("lega");
-        binding.titoloLega.setText(lega.getNomeLega());
 
-        binding.tabView.setSelectedItemId(R.id.home);
+        binding.tabView.setSelectedItemId(R.id.classifica);
         binding.tabView.setOnItemSelectedListener(item -> {
             int itemId = item.getItemId();
             if (itemId == R.id.home) {
-                Intent intent = new Intent(LegaActivity.this, LegaActivity.class);
+                Intent intent = new Intent(ClassificaActivity.this, LegaActivity.class);
                 intent.putExtra("lega", lega);
                 startActivity(intent);
                 return true;
             } else if (itemId == R.id.calendario) {
-                Intent intent = new Intent(LegaActivity.this, CalendarioActivity.class);
+                Intent intent = new Intent(ClassificaActivity.this, CalendarioActivity.class);
                 intent.putExtra("lega", lega);
                 startActivity(intent);
                 return true;
             } else if (itemId == R.id.classifica) {
-                Intent intent = new Intent(LegaActivity.this, ClassificaActivity.class);
-                intent.putExtra("lega", lega);
-                startActivity(intent);
+                Intent intent = new Intent(ClassificaActivity.this, ClassificaActivity.class);
+                intent.putExtra("lega", lega);                startActivity(intent);
                 return true;
             }
             return false;
